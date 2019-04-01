bad4a0255f781bceaa25d1252b009ef6aa2fc807
reazeal <awaluddin.rizal@gmail.com>
2019-04-01 13:59:58 +0700

    update query komisi

--- application/models/Transaksi_biaya_model.php
+++ application/models/Transaksi_biaya_model.php
@@ -183,8 +183,9 @@ class Transaksi_biaya_model extends MY_Model
         $this->db->join("piutang b","a.kode_piutang = b.kode_referensi");
         $this->db->join("detail_so c","c.kode_so = b.kode_referensi");
         $this->db->join("sales_order d","d.kode_so = c.kode_so");
-        $this->db->join("pegawai e","e.kode_pegawai = d.kode_sales");
+        
         $this->db->join("pengiriman_so f","f.kode_so = d.kode_so");
+        $this->db->join("pegawai e","e.kode_pegawai = f.kode_kurir");
         $this->db->where(" month(d.tanggal)='$bulan' and year(d.tanggal) = '$tahun' and e.jabatan != 'owner' ");
         $this->db->group_by("kode_sales");
         $query = $this->db->get('pembayaran_piutang a');
